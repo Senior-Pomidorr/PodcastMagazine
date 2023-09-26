f53515c32b5df5b49b32f81b69e68017319e5944
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2023-09-26 20:17:43 +0300

    add FavoriteCell

---
+++ PodcastMagazine/Resources/FavoritesCell.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesCell.swift
+//  PodcastMagazine
+//
+//  Created by Daniil Kulikovskiy on 26.09.2023.
+//
+
+import SwiftUI
+
+struct FavoritesCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesCell()
+}
---
+++ PodcastMagazine/Screens/HomePageView/Playlist.swift
@@ -0,0 +1,18 @@
+//
+//  Playlist.swift
+//  PodcastMagazine
+//
+//  Created by Daniil Kulikovskiy on 26.09.2023.
+//
+
+import SwiftUI
+
+struct Playlist: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Playlist()
+}
