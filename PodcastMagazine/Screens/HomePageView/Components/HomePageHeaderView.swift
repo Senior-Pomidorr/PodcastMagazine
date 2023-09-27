640576073088b64d41ec92e5c47ffd5eadfed8a3
Alexandr Rodionov <alexrodionby@gmail.com>
2023-09-28 01:03:39 +0300

    add new colours and app icon

--- PodcastMagazine/Screens/HomePageView/Components/HomePageHeaderView.swift
+++ PodcastMagazine/Screens/HomePageView/Components/HomePageHeaderView.swift
@@ -9,11 +9,24 @@ import SwiftUI
 
 struct HomePageHeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-  
+        HStack {
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Alexandr Rodionov")
+                    .font(.custom(.bold, size: 16))
+                    .foregroundStyle(Color.black)
+                Text("Live, write code, die")
+                    .font(.custom(.light, size: 14))
+                    .foregroundStyle(Color.gray)
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: 12)
+                .frame(width: 48, height: 48)
+                .foregroundStyle(Color.color1)
+                .shadow(color: .black.opacity(0.3), radius: 10, x: 0, y: 10)
+        }
     }
 }
 
 #Preview {
-    HomePageHeaderView()
+    HomePageView()
 }
