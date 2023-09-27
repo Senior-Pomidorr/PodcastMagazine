27d888a5bcb0e4d6bf3ef28445d2bce8e776b998
Alexandr Rodionov <alexrodionby@gmail.com>
2023-09-27 17:42:22 +0300

    working on the home screen

--- PodcastMagazine/Screens/HomePageView/HomePageDomain.swift
+++ PodcastMagazine/Screens/HomePageView/HomePageDomain.swift
@@ -10,7 +10,7 @@ import Models
 import Combine
 
 // MARK: - HomePageLoadingStatus
-private enum HomePageLoadingStatus: Equatable {
+enum HomePageLoadingStatus: Equatable {
     static func == (lhs: HomePageLoadingStatus, rhs: HomePageLoadingStatus) -> Bool {
         String(describing: lhs) == String(describing: rhs)
     }
@@ -20,107 +20,124 @@ private enum HomePageLoadingStatus: Equatable {
     case error(Error)
 }
 
-// MARK: - State
-private struct State: Equatable {
-    var categoryList: [Models.Category]
-    var podcastsList: [Feed]
-    var homePageLoadingStatus: HomePageLoadingStatus
-    
-    init(
-        categoryList: [Models.Category] = .init(),
-        podcastsList: [Feed] = .init(),
-        homePageLoadingStatus: HomePageLoadingStatus = .none
-    ) {
-        self.categoryList = categoryList
-        self.podcastsList = podcastsList
-        self.homePageLoadingStatus = homePageLoadingStatus
+struct HomePageDomain {
+    // MARK: - State
+    struct State: Equatable {
+        var categoryList: [Models.Category]
+        var podcastsList: [Feed]
+        var homePageLoadingStatus: HomePageLoadingStatus
+        
+        init(
+            categoryList: [Models.Category] = .init(),
+            podcastsList: [Feed] = .init(),
+            homePageLoadingStatus: HomePageLoadingStatus = .none
+        ) {
+            self.categoryList = categoryList
+            self.podcastsList = podcastsList
+            self.homePageLoadingStatus = homePageLoadingStatus
+        }
     }
-}
-
-// MARK: - Action
-enum Action {
-    case viewAppeared
-    case categoryCellDidTap
-    case seeAllButtonDidTap
-    case _getCategoryRequest
-    case _getPodcastRequest
-    case _getCategoryResponse(Result<[Models.Category], Error>)
-    case _getPodcastsResponse(Result<[Feed], Error>)
-}
-
-// MARK: - Dependencies
-struct Dependencies {
+    
+    // MARK: - Action
+    enum Action {
+        case viewAppeared
+        case categoryCellDidTap
+        case _getCategoryRequest
+        case _getPodcastRequest
+        case _getCategoryResponse(Result<[Models.Category], Error>)
+        case _getPodcastsResponse(Result<[Feed], Error>)
+    }
+    
+    // MARK: - Dependencies
     let getCategoryList: (String) -> AnyPublisher<[Models.Category], Error>
     let getPodcastsList: (String) -> AnyPublisher<[Feed], Error>
-}
-
-// MARK: - Reduce
-fileprivate func reduce(
-    _ state: inout State,
-    with action: Action,
-    env: Dependencies
-) -> AnyPublisher<Action, Never> {
-    switch action {
-        
-    case ._getCategoryRequest:
-        return env.getCategoryList("categoryUrl")
-            .map(toSuccessCategory(_:))
-            .catch(toFailCategory(_:))
+    
+    // MARK: - Reducer
+    func reduce(
+        _ state: inout State,
+        with action: Action
+    ) -> AnyPublisher<Action, Never> {
+        switch action {
+            
+        case .viewAppeared:
+            guard state.homePageLoadingStatus != .loading else { break }
+            state.homePageLoadingStatus = .loading
+            
+            return Publishers.Merge(
+                Just(._getCategoryRequest),
+                Just(._getPodcastRequest)
+            )
             .eraseToAnyPublisher()
-        
-    case ._getPodcastRequest:
-        return env.getPodcastsList("podcastsUrl")
-            .map(toSuccessFeed(_:))
-            .catch(toFailFeed(_:))
-            .eraseToAnyPublisher()
-        
-    case .viewAppeared:
-        guard state.homePageLoadingStatus != .loading else { break }
-        state.homePageLoadingStatus = .loading
-        
-        return Publishers.Merge(
-            Just(._getCategoryRequest),
-            Just(._getPodcastRequest)
-        )
-            .eraseToAnyPublisher()
-        
-    case let ._getCategoryResponse(.success(apiCategories)):
-        state.homePageLoadingStatus = .none
-        state.categoryList = apiCategories
-        
-    case let ._getPodcastsResponse(.success(feeds)):
-        state.homePageLoadingStatus = .none
-        state.podcastsList = feeds
-        
-    case let ._getCategoryResponse(.failure(error)), let ._getPodcastsResponse(.failure(error)):
-        state.homePageLoadingStatus = .error(error)
-        
-    case .categoryCellDidTap:
-        break
-        
-    case .seeAllButtonDidTap:
-        break
-
+            
+        case ._getCategoryRequest:
+            return getCategoryList("categoryUrl")
+                .map(toSuccessCategory(_:))
+                .catch(toFailCategory(_:))
+                .eraseToAnyPublisher()
+            
+        case ._getPodcastRequest:
+            return getPodcastsList("podcastsUrl")
+                .map(toSuccessFeed(_:))
+                .catch(toFailFeed(_:))
+                .eraseToAnyPublisher()
+            
+        case let ._getCategoryResponse(.success(apiCategories)):
+            state.homePageLoadingStatus = .none
+            state.categoryList = apiCategories
+            
+        case let ._getPodcastsResponse(.success(feeds)):
+            state.homePageLoadingStatus = .none
+            state.podcastsList = feeds
+            
+        case let ._getCategoryResponse(.failure(error)), let ._getPodcastsResponse(.failure(error)):
+            state.homePageLoadingStatus = .error(error)
+            
+        case .categoryCellDidTap:
+            break
+            
+        }
+        return Empty().eraseToAnyPublisher()
     }
-    return Empty().eraseToAnyPublisher()
+    
+    // MARK: - SuccessActionCategory
+    func toSuccessCategory(_ apiCategories: [Models.Category]) -> Action {
+        ._getCategoryResponse(.success(apiCategories))
+    }
+    
+    // MARK: - FailActionCategory
+    func toFailCategory(_ error: Error) -> Just<Action> {
+        Just(._getCategoryResponse(.failure(error)))
+    }
+    
+    // MARK: - SuccessActionFeed
+    func toSuccessFeed(_ feeds: [Feed]) -> Action {
+        ._getPodcastsResponse(.success(feeds))
+    }
+    
+    // MARK: - FailActionFeed
+    func toFailFeed(_ error: Error) -> Just<Action> {
+        Just(._getPodcastsResponse(.failure(error)))
+    }
 }
 
-// MARK: - SuccessActionCategory
-func toSuccessCategory(_ apiCategories: [Models.Category]) -> Action {
-    ._getCategoryResponse(.success(apiCategories))
-}
-
-// MARK: - FailActionCategory
-func toFailCategory(_ error: Error) -> Just<Action> {
-    Just(._getCategoryResponse(.failure(error)))
-}
-
-// MARK: - SuccessActionFeed
-func toSuccessFeed(_ feeds: [Feed]) -> Action {
-    ._getPodcastsResponse(.success(feeds))
-}
-
-// MARK: - FailActionFeed
-func toFailFeed(_ error: Error) -> Just<Action> {
-    Just(._getPodcastsResponse(.failure(error)))
+// MARK: - HomePageStore
+final class HomePageStore: ObservableObject {
+    @Published private(set) var state: HomePageDomain.State
+    private let reducer: (inout HomePageDomain.State, HomePageDomain.Action) -> AnyPublisher<HomePageDomain.Action, Never>
+    private var cancellable: Set<AnyCancellable> = .init()
+    
+    init(
+        state: HomePageDomain.State,
+        reducer: @escaping (inout HomePageDomain.State, HomePageDomain.Action) -> AnyPublisher<HomePageDomain.Action, Never>
+    ) {
+        self.state = state
+        self.reducer = reducer
+    }
+    
+    func send(_ action: HomePageDomain.Action) {
+        reducer(&state, action)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: send(_:))
+            .store(in: &cancellable)
+    }
 }
--- PodcastMagazine/Screens/HomePageView/HomePageView.swift
+++ PodcastMagazine/Screens/HomePageView/HomePageView.swift
@@ -6,12 +6,42 @@
 //
 
 import SwiftUI
+import Models
 
 struct HomePageView: View {
+
+   // @State var homePageViewLoadingStatus: HomePageLoadingStatus = .none
+    @StateObject var store: HomePageStore = HomePageStore(
+        state: HomePageDomain.State(),
+        reducer: HomePageDomain(
+            getCategoryList: <#(String) -> AnyPublisher<[Category], Error>#>,
+            getPodcastsList: <#(String) -> AnyPublisher<[Feed], Error>#>
+        ).reduce(_:with:)
+    )
+    
     var body: some View {
-        GeometryReader { geometry in
-            Text("Hello")
+        
+        switch homePageViewLoadingStatus {
+        case .none:
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 20) {
+                    ForEach(1..<11) { item in
+                        CategoryCellView(categoryCellData: Models.Category(id: item, name: ""))
+                    }
+                }
+                .padding()
+            }
+        case .loading:
+            ProgressView()
+        case let .error(error):
+            VStack {
+                Text(error.localizedDescription)
+                Image(systemName: "wifi.slash")
+                    .frame(width: 100, height: 100, alignment: .center)
+            }
         }
+        
+
     }
 }
 
