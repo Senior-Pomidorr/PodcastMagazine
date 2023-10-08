747fc05d598c265c326287fa7b01ae16eff175e3
Ilia Shapovalov <greateman@mail.ru>
2023-10-08 12:35:13 +0300

    Add Registration logic. Add Tests

--- Repository/Sources/RegistrationDomain/RegistrationDomain.swift
+++ Repository/Sources/RegistrationDomain/RegistrationDomain.swift
@@ -16,18 +16,45 @@ public struct RegistrationDomain: ReducerDomain {
     //MARK: - State
     public struct State: Equatable {
         public var email: String
+        public var firstName: String
+        public var lastName: String
+        public var password: String
+        
+        public var isEmailValid: Bool
+        public var isPasswordValid: Bool
+        public var completeAccountPresented: Bool
         
         //MARK: - init(_:)
         public init(
-            email: String = .init()
+            email: String = .init(),
+            firstName: String = .init(),
+            lastName: String = .init(),
+            password: String = .init(),
+            isEmailValid: Bool = true,
+            isPasswordValid: Bool = true,
+            completeAccountPresented: Bool = false
         ) {
             self.email = email
+            self.firstName = firstName
+            self.lastName = lastName
+            self.password = password
+            self.isEmailValid = isEmailValid
+            self.isPasswordValid = isPasswordValid
+            self.completeAccountPresented = completeAccountPresented
         }
     }
     
     //MARK: - Action
     public enum Action: Equatable {
         case setEmail(String)
+        case setFirstName(String)
+        case setLastName(String)
+        case setPassword(String)
+        case continueButtonTap
+        case dismissCompleteAccount
+        case signUpButtonTap
+        case _createNewUserRequest
+        case _invalidCredentials
     }
     
     //MARK: - Dependencies
@@ -47,6 +74,56 @@ public struct RegistrationDomain: ReducerDomain {
     public func reduce(_ state: inout State, action: Action) -> AnyPublisher<Action, Never> {
         switch action {
         case let .setEmail(email):
+            switch email.isEmpty {
+            case true:
+                state.email = .init()
+                state.isEmailValid = true
+                
+            case false:
+                state.email = email
+                state.isEmailValid = validateEmail(email)
+            }
+            
+        case let .setPassword(password):
+            switch password.isEmpty {
+            case true:
+                state.password = .init()
+                state.isPasswordValid = true
+                
+            case false:
+                state.password = password
+                state.isPasswordValid = validatePassword(password)
+            }
+            
+        case let .setFirstName(firstName):
+            state.firstName = firstName.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+        case let .setLastName(lastName):
+            state.lastName = lastName.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+        case .continueButtonTap:
+            state.completeAccountPresented = true
+            
+        case .dismissCompleteAccount:
+            state.completeAccountPresented = false
+            
+        case .signUpButtonTap:
+            guard
+                state.isEmailValid,
+                state.isPasswordValid,
+                !state.email.isEmpty,
+                !state.password.isEmpty,
+                !state.firstName.isEmpty,
+                !state.lastName.isEmpty
+            else {
+                return run(._invalidCredentials)
+            }
+            return run(._createNewUserRequest)
+            
+        case ._invalidCredentials:
+            break
+            
+        case ._createNewUserRequest:
             break
         }
         return empty()
